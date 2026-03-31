a1105e2ec44e59e0e27fd5228a386bdc03852911
rscoder999 <rscodes999@gmail.com>
2026-03-31 12:14:55 +0530

    Update bank_account.py

--- bank_account.py
+++ bank_account.py
@@ -1,10 +1,22 @@
-#here salary attribute is a private member which can be accessed only within the class
-class Employee:
-    def __init__(self, salary):
-        self.__salary = salary
+class BankAccount:
 
-    def get_salary(self):
-        return self.__salary
+    def __init__(self,balance):
+        self.balance=balance
 
-emp = Employee(50000)
-print(emp.get_salary())
+    def deposit(self,amount):
+        self.balance+=amount
+
+    def withdraw(self,amount):
+        self.balance-=amount
+
+    def show_balance(self):
+        print("Balance:",self.balance)
+
+
+account=BankAccount(1000)
+account.deposit(500)
+account.withdraw(200)
+account.show_balance()
+
+        
+        
